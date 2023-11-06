cb88fde2fafa0aaa06dbba8618f33be495ba56f3
Kvarutar <48532279+Kvarutar@users.noreply.github.com>
2023-11-06 22:35:10 +0300

    [Task-48] i18n - install library to support i18n (#8)

---
+++ src/core/locales/i18n.config.ts
@@ -0,0 +1,15 @@
+import { ru, en, he } from './translations';
+
+const resources = {
+    en: {
+        translation: en, 
+    },
+    ru: {
+        translation: ru, 
+    },
+    he: { 
+        translation: he, 
+    },
+};
+
+export default resources;
