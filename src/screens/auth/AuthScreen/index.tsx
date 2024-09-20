9eb883f1a21407fcf0f1f64adbcd22217742a7e9
Vladyslav Lisnenko <vladyslav.lisnenko@shuttlex.com>
2024-09-20 20:38:33 +0300

    [TASK-199] Passenger - Auth - “Sign in” screen (#111)
    * [FEAT] - modify Sing In Screen
    
    * [FEAT] - add ScrollViewWithCustomScroll
    
    * [FIX] - some changes
    
    * [FEAT] - delete old 'sign in' screen
    
    * [FEAT] - add sign in screen from integration
    
    * [FEAT] - function for getting data from integration
    
    * [FEAT] - test functionality (not completed)
    
    * [FIX] - functionality
    
    * [FEAT] - add package-lock.json and package.json
    
    * [FIX] - comments
    
    * fix
    
    ---------
    
    Co-authored-by: Nikolai141 <nikolai.balantsev@shuttlex.com>

--- src/screens/auth/AuthScreen/index.tsx
+++ src/screens/auth/AuthScreen/index.tsx
@@ -1,30 +1,45 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { SafeAreaView, SignUpScreen } from 'shuttlex-integration';
+import { SafeAreaView, SignInScreen, SignUpForm, SignUpScreen, SignUpScreenRef } from 'shuttlex-integration';
 
 import { AuthScreenProps } from './props';
-import SignIn from './SignIn';
 import TitleWithCloseButton from './TitleWithCloseButton';
 
 const AuthScreen = ({ navigation, route }: AuthScreenProps): JSX.Element => {
   const [isSignIn, setIsisSignIn] = useState<boolean>(route.params.state === 'SignIn');
   const { t } = useTranslation();
+  const signUpRef = useRef<SignUpScreenRef>(null);
+
+  const handleSendingSignUpData = (dataForm: SignUpForm) => {
+    //TODO add logic for send data on back
+    if (signUpRef.current && dataForm.email === 'qwe@qwe.com') {
+      signUpRef.current.showErrors({ email: 'Email is already in use' });
+      return;
+    }
+    navigation.navigate('SignInPhoneCode');
+  };
+
+  const handleSendingSignInData = (phone: string) => {
+    //TODO add logic for send data on back
+    if (phone) {
+      navigation.navigate('SignInPhoneCode');
+    }
+  };
 
   return (
     <SafeAreaView>
       <TitleWithCloseButton
-        title={
-          isSignIn ? t('auth_Auth_HeaderWithCloseButton_signInTitle') : t('auth_Auth_HeaderWithCloseButton_signUpTitle')
-        }
+        title={isSignIn ? t('auth_Auth_signInTitle') : t('auth_Auth_signUpTitle')}
         onBackButtonPress={() => navigation.replace('Splash')}
       />
       {isSignIn ? (
-        <SignIn onPress={() => setIsisSignIn(false)} navigation={navigation} />
+        <SignInScreen navigateToSignUp={() => setIsisSignIn(false)} onSubmit={handleSendingSignInData} />
       ) : (
         <SignUpScreen
+          ref={signUpRef}
           navigateToSignIn={() => setIsisSignIn(true)}
           navigateToTerms={() => navigation.navigate('Terms')}
-          navigateToSingUpCode={() => navigation.navigate('SignInPhoneCode')}
+          onSubmit={handleSendingSignUpData}
         />
       )}
     </SafeAreaView>
--- src/screens/auth/AuthScreen/props.ts
+++ src/screens/auth/AuthScreen/props.ts
@@ -1,23 +1,5 @@
-import { NativeStackNavigationProp, NativeStackScreenProps } from '@react-navigation/native-stack';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
 
 import { RootStackParamList } from '../../../Navigate/props';
 
 export type AuthScreenProps = NativeStackScreenProps<RootStackParamList, 'Auth'>;
-
-export type SignProps = {
-  onPress: () => void;
-  navigation: NativeStackNavigationProp<RootStackParamList, 'Auth', undefined>;
-};
-
-export type SignInPhoneStateProps = {
-  onLabelPress: () => void;
-  changePhoneNumber: (phoneNumber: string | null) => void;
-  isCorrectPhoneNumber?: boolean;
-  navigation: NativeStackNavigationProp<RootStackParamList, 'Auth', undefined>;
-};
-
-export type SignInEmailStateProps = {
-  onLabelPress: () => void;
-  changeEmail: (phoneNumber: string) => void;
-  isCorrectEmail: boolean;
-};
