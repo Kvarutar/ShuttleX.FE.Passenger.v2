2c3806ac5320d24689232490377eda3160b354d5
Aleksandr-Voronchikhin <aleksandr.voronchikhin@shuttlex.com>
2025-02-21 12:21:45 +0300

    [Task-remove-uk] (#412)
    Co-authored-by: Nikolai141 <nikolai.balantsev@shuttlex.com>

--- src/core/locales/i18n.ts
+++ src/core/locales/i18n.ts
@@ -1,6 +1,5 @@
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import { getLocales } from 'react-native-localize';
 import i18nIntegration from 'shuttlex-integration/src/core/locales/i18n';
 
 import resources from './i18n.config';
@@ -9,15 +8,15 @@ i18next
   .use(initReactI18next)
   .init({
     compatibilityJSON: 'v3',
-    lng: getLocales()[0].languageCode,
+    lng: 'en',
     resources,
-    supportedLngs: ['en', 'uk', 'ru'],
+    supportedLngs: ['en'],
     fallbackLng: 'en',
     interpolation: {
       escapeValue: false,
     },
   })
-  .then(() => i18nIntegration.changeLanguage(getLocales()[0].languageCode));
+  .then(() => i18nIntegration.changeLanguage('en'));
 
 i18next.on('languageChanged', lng => {
   i18nIntegration.changeLanguage(lng);
